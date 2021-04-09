b255aba4cb333cf842ad97ae4be8e66df5c09c91
YumiChann <TARRISSELINOA@aol.com>
2021-04-09 12:58:03 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@tailwindcss/forms')
+],
+}
